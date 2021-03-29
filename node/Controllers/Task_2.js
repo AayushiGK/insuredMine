0ac91b0daaada8cba9864bab2e2cfad1efc0cafb
Aayushi <aayushicumar@gmail.com>
2021-03-29 12:39:08 +0530

    Task 2 ii done

--- node/Controllers/Task_2.js
+++ node/Controllers/Task_2.js
@@ -2,9 +2,6 @@
 // 1) Track real-time CPU utilization of the node server and on 70% usage restart the server.
 // 2) Create a post-service that takes the message, day, and time in body parameters and it inserts that message into DB at that particular day and time.
 
-
-// Task 2 => 2 ) This task is independent of the above task. You can have two collections collection and collection 2. On post request, a message and timestamp will get saved into the collection and a job will be scheduled on the timestamp which transfers the message from collection 1 to collection 2
-
 var router = require("express").Router();
 var schedule = require('node-schedule');
 module.exports = function (arrg) {
@@ -16,6 +13,7 @@ module.exports = function (arrg) {
         let { message, date, time } = req.body;
         let date = new Date(date);
         let rule = new schedule.RecurrenceRule();
+        rule.year = date.getFullYear();
         rule.month = date.getMonth();
         rule.date = date.getDate();
         rule.hour = time.getHour();
@@ -29,5 +27,31 @@ module.exports = function (arrg) {
             })
         })
     })
+
+
+
+    // Task 2 => ii ) This task is independent of the above task. You can have two collections collection and collection 2. On post request, a message and timestamp will get saved into the collection and a job will be scheduled on the timestamp which transfers the message from collection 1 to collection 2
+
+    router.post("/shiftingMessage", (req, res, next) => {
+        const { message, timestamp } = req.body;
+
+        let time_given = new Date(timestamp);
+
+        let rule = new schedule.RecurrenceRule();
+        rule.year = time_given.getFullYear();
+        rule.month = time_given.getMonth();
+        rule.date = time_given.getDate();
+        rule.hour = time_given.getHour();
+        rule.minute = time_given.getMinute();
+
+        Collection_One.create({ message, timestamp }).then(data_in_collection_1 => {
+            schedule.scheduleJob(rule, function () {
+                Collection_Two.create({ 'message': data_in_collection_1.message });
+                Collection_one.findOneAndRemove({ 'message': data_in_collection_1.message });
+            })
+        }).catch(() => {
+            return res.status(422).send({ content: "Can't create the message" });
+        });
+    });
     return router;
 }
